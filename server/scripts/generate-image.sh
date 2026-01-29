fe96fb4c059dfe289299b6bcafe1c9663da08a18
Marianne Feng <feng.marianne@gmail.com>
2026-01-29 15:37:24 -0500

    change width height in wkhtmltoimage command

--- server/scripts/generate-image.sh
+++ server/scripts/generate-image.sh
@@ -5,4 +5,4 @@ PORT=${PORT:-3000}
 OUTPUT="${SCREEN_PNG_PATH:-/app/screen.png}"
 URL="http://localhost:${PORT}/html"
 
-wkhtmltoimage --disable-javascript --no-images --quality 85 --width 600 --height 800 "$URL" "$OUTPUT" 2>/dev/null && convert "$OUTPUT" -rotate -90 "$OUTPUT" 2>/dev/null
+wkhtmltoimage --disable-javascript --no-images --quality 85 --width 800 --height 600 "$URL" "$OUTPUT" 2>/dev/null && convert "$OUTPUT" -rotate -90 "$OUTPUT" 2>/dev/null
